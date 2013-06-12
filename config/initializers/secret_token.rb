17cb083031e6d9a79ee9f7c642cdd794235d8328
Sam Hoefer <samh@brushfireinteractive.com>
2013-06-12 13:52:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseApp::Application.config.secret_token = '23f79901cdba60ee1884c74a47e11d6bb4bb1c6cc5cc547f373a0b56e7af5fdc88375b09db40d580fe24c6420fcf5419588d648379975210fb6bd9890172667e'
